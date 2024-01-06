bd9254d5ecca5338b3cfddde1924414dd01875df
jsr <jayshreer130@gmail.com>
2024-01-06 20:17:11 +0530

    made a button in main.js

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,16 +1,20 @@
 import React from "react";
-import './Main.css'
+import "./Main.css";
 function MainContent() {
-    return(
-        
-        <div className="d-flex align-items-center justify-content-center m-0" style={{ height: "100vh" }}>
-            <div className="container text-center text-white m-0">
-                <p>ADVENTURE AWAITS</p>
-                <h3>What are you waiting for?</h3>
-                <h2/>
-            </div>
+  return (
+    <div
+      className="d-flex align-items-center justify-content-center m-0"
+      style={{ height: "100vh" }}
+    >
+      <div className="container text-center text-white m-0">
+        <p>ADVENTURE AWAITS</p>
+        <h3>What are you waiting for?</h3>
+        <div className="d-flex align-items-center justify-content-center my-3">
+          <button className="btn btn-secondary  mx-2">Get Started</button>
+          <button className="btn btn-secondary mx-2">Watch Trailer</button>
         </div>
-        
-    );
+      </div>
+    </div>
+  );
 }
 export default MainContent;
